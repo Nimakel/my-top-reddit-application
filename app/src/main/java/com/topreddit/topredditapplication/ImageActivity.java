d093638e0992e60e35ff5702abdd71647228c44e
Nimakel <maxim.yashyn@gmail.com>
2023-06-08 10:06:51 +0300

    implemented image loader support and removed redundant button

--- app/src/main/java/com/topreddit/topredditapplication/ImageActivity.java
+++ app/src/main/java/com/topreddit/topredditapplication/ImageActivity.java
@@ -1,7 +1,8 @@
 package com.topreddit.topredditapplication;
 
+import com.topreddit.topredditapplication.adapter.PostViewAdapter;
+import com.topreddit.topredditapplication.data.ImageLoader;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,7 +12,7 @@ import androidx.annotation.Nullable;
 public class ImageActivity extends Activity {
     private ImageView imageView;
     private Button saveButton;
-    private Button returnButton;
+    private ImageLoader imageLoader;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,12 +20,13 @@ public class ImageActivity extends Activity {
         setContentView(R.layout.image_layout);
         imageView = findViewById(R.id.imageView);
         saveButton = findViewById(R.id.save_button);
-        returnButton = findViewById(R.id.return_button);
-        returnButton.setOnClickListener(new View.OnClickListener() {
+        imageLoader = new ImageLoader();
+        imageView.setImageBitmap(imageLoader.getBitmapFromUrl(PostViewAdapter.getImage()));
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ImageActivity.this, MainActivity.class);
-                startActivity(intent);
+
             }
         });
     }
